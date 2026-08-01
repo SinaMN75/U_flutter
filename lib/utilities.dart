39a889f090b786d6b0aa0dc58c9e495d74cb2496
Sina MohammadZadeh <sina75mn@outlook.com>
2026-08-01 08:43:19 +0330

    :)

--- lib/utilities.dart
+++ lib/utilities.dart
@@ -152,4 +152,4 @@ export "utils/u_utils.dart";
 export "utils/web/u_pwa.dart";
 export "utils/web/u_web_message.dart";
 
-export "plugins/screen_guard/screen_guard.dart";
+export "plugins/screen_guard.dart";
