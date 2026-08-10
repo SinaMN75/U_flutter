8741807623754de9d278adaa059f89c7088cee7c
Sina MohammadZadeh <sina75mn@outlook.com>
2026-08-10 21:12:15 +0330

    :)

--- lib/data/payment/ipg_web_view_page.dart
+++ lib/data/payment/ipg_web_view_page.dart
@@ -26,7 +26,7 @@ class _UIpgWebViewPageState extends State<UIpgWebViewPage> {
     },
     child: UScaffold(
       appBar: AppBar(title: Text(U.s.payment)),
-      body: UWebView(initialUrl: widget.url, showUrlBar: true),
+      body: UWebView(initialUrl: widget.url, showUrlBar: true, onPageFinished: c.onPageFinished),
     ),
   );
 }
